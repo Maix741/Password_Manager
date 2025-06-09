e626db864d73eadf4d522e9367ebc3a4d1e3b4b6
Maix741 <maxi2018milian@outlook.de>
2025-06-09 15:48:41 +0200

    refactor: consolidate imports in src/__init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -2,5 +2,4 @@ from .manager_cmd import ManagerCMD
 from .manager_gui import ManagerGUI
 
 # import necessary utils
-from .search_passwords import search_passwords
-from .remove_password import remove_password
+from .utils import search_passwords, remove_password
