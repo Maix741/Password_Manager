1e353c2b517d2211d91af6c7c498b05f7237a31b
Maix741 <maxi2018milian@outlook.de>
2025-07-11 20:42:25 +0200

    fix: use get method for safer access to website key in get_website_for_password function

--- src/utils/get_website_for_password.py
+++ src/utils/get_website_for_password.py
@@ -10,7 +10,8 @@ def get_website_for_password(passwords_path: str, password_name: str) -> str:
             password: dict[str, str] = json.load(password_file_object)
 
         logging.debug(f"Got website for password: {password_name}")
-        return password["website"] if password["website"] else "n/a"
+        return password.get("website", "n/a")
 
     except (PermissionError, FileNotFoundError, KeyError) as exception:
         logging.error(f"Error while getting website for password ({password_name}: {exception})")
+        return "n/a"
