da7dd1586673045ad3190ecbbf5cc514fa94fe53
Maix741 <maxi2018milian@outlook.de>
2025-07-16 11:05:24 +0200

    fix: add error handling and logging in get_data_path function for improved robustness

--- src/utils/get_data_path.py
+++ src/utils/get_data_path.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import platform
+import logging
 import os
 
 from .setup_folders import setup_folders
@@ -8,8 +9,12 @@ from .setup_folders import setup_folders
 def get_data_path() -> str:
     data_path: str = get_path()
 
-    setup_folders(data_path)
-
+    try:
+        setup_folders(data_path)
+    except (FileNotFoundError, PermissionError) as e:
+        logging.error(f"\"{data_path}\" could not be used because of: {e}")
+        data_path: str = custom_path_not_available()
+        setup_folders(data_path)
     return data_path
 
 
@@ -17,6 +22,14 @@ def get_path() -> str:
     if os.getenv("PASSWORD_MANAGER_DATA_PATH"):
         return os.getenv("PASSWORD_MANAGER_DATA_PATH")
 
+    if platform.system() == "Windows":
+        return os.path.join(os.getenv("LOCALAPPDATA"), "Password_manager")
+
+    else:
+        return os.path.join(Path.home(), ".Password_manager")
+
+
+def custom_path_not_available() -> str:
     if platform.system() == "Windows":
         return os.path.join(os.getenv("LOCALAPPDATA"), "Password_manager")
 
