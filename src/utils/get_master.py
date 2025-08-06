395e81abe6cceff1341a831e64d69c92227b526c
Maix741 <maxi2018milian@outlook.de>
2025-08-06 23:03:36 +0200

    fix: fix retrieving master password iterations

--- src/utils/get_master.py
+++ src/utils/get_master.py
@@ -6,9 +6,9 @@ def get_master(data_path: str) -> list[bytes, bytes, int]:
     master_file: str = os.path.join(data_path, "master", "master_pass.pem")
     try:
         with open(master_file, "r") as file:
-            master: str = file.read() # hash, salt, iterations
-            master = [bytes.fromhex(k) for k in master.split("\n")]
-            master[2] = int(master[2])
+            master_raw: str = file.read() # hash, salt, iterations
+            master = [bytes.fromhex(k) for k in master_raw.split("\n")[:2]]
+            master.append(int(master_raw.split("\n")[2]))
 
     except (FileNotFoundError, PermissionError, ValueError) as e:
         logging.error(f"Error getting master from file: {e}")
