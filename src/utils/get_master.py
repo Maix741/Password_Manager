85fbc02d7fba7f219d6c5cd661aafa26f5c7679d
Maix741 <maxi2018milian@outlook.de>
2025-08-06 20:19:04 +0200

    refactor: move get_master to a seperate file

---
+++ src/utils/get_master.py
@@ -0,0 +1,16 @@
+import logging
+import json
+import os
+
+
+def get_master(data_path: str) -> list[bytes, bytes, int]:
+    master_file: str = os.path.join(data_path, "master", "master_pass.pem")
+    try:
+        with open(master_file, "r") as file:
+            master: dict[bytes, bytes, int] = json.loads(file.read()) # hash, salt, iterations
+
+    except (FileNotFoundError, PermissionError) as e:
+        logging.error(f"Error getting master from file: {e}")
+        return None
+
+    return list(master.values())
